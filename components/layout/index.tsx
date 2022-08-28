1da6156946995a59a8826294b4e66ddedc33dbd5
DesmondTambe <desking2000@gmail.com>
2022-08-28 14:21:08 +0100

    feat: Authcontext api for user login using email and password

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -3,30 +3,39 @@ import React, { createContext } from "react";
 import Navbar from "./navbar";
 import Page from "./page";
 import Sidebar from "./sidebar";
+import Cookies from "js-cookie";
 import Scroll from "./scroll";
 import { useDisclosure, UseDisclosureReturn } from "@chakra-ui/hooks";
 import MobileSidebar from "./sidebar/mobile";
 import { useMediaQuery } from "@chakra-ui/media-query";
+import Login from "pages/login";
 
 export const NavContext = createContext<UseDisclosureReturn>(null);
 
 const SiteLayout = ({ children }: any) => {
   const sidebarState = useDisclosure();
   const [isSmallScreen] = useMediaQuery("(max-width: 768px)");
+
+  let allowed = Cookies.get("access_token") ? true : false;
+
   return (
     <Scroll>
-      <NavContext.Provider value={sidebarState}>
-        <Box textStyle="light">
-          <Navbar />
-          <Box pos="relative" h="max-content" m={[2, , 5]}>
-            <Stack direction="row" spacing={{ md: 5 }}>
-              <Sidebar />
-              {isSmallScreen && <MobileSidebar />}
-              <Page>{children}</Page>
-            </Stack>
+      {/* {allowed ? ( */}
+        <NavContext.Provider value={sidebarState}>
+          <Box textStyle="light">
+            <Navbar />
+            <Box pos="relative" h="max-content" m={[2, , 5]}>
+              <Stack direction="row" spacing={{ md: 5 }}>
+                <Sidebar />
+                {isSmallScreen && <MobileSidebar />}
+                <Page>{children}</Page>
+              </Stack>
+            </Box>
           </Box>
-        </Box>
-      </NavContext.Provider>
+        </NavContext.Provider>
+      {/* // ) : (
+      //   <Login />
+      // )} */}
     </Scroll>
   );
 };
